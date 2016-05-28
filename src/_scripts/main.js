bd716911e56e15fc8175a1b16b820a4dbb344a55
Michael <michael.lai@adelphi.digital>
2016-05-28 17:00:26 +1000

    First commit

---
+++ src/_scripts/main.js
@@ -0,0 +1,18 @@
+// Main javascript entry point
+// Should handle bootstrapping/starting application
+
+'use strict';
+
+// Importing it in the base.jade because importing jquery here is going to the end of the file
+//import $ from 'jquery';
+
+import foundation from 'foundation-sites';
+
+import Header from '../_modules/header/header';
+import Footer from '../_modules/footer/footer';
+
+$(() => {
+	
+	
+
+});
