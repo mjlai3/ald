78c1de4bd223a2dc0eac4ea8bc59c55d0cfd4c18
Michael <michael.lai@adelphi.digital>
2016-06-12 00:51:05 +1000

    Weird ordering not working

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -22,8 +22,10 @@ $(() => {
 	var grid = $('.grid');
 
 	grid.masonry({
-		itemSelector: '.grid-item',
-		columnWidth: 200
+	    itemSelector: '.grid-item',
+	    columnWidth: '.grid-sizer',
+	    gutter: '.gutter-sizer',
+	    percentPosition: true
 	});
 
 	grid.infinitescroll({
