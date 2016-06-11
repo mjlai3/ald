f9a788b723de2da3b55dc0946e36f629fcd26d20
Michael <michael.lai@adelphi.digital>
2016-06-11 13:38:36 +1000

    Add masonry and infinite scroll

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -19,4 +19,40 @@ $(() => {
 	    centerMode: true
 	});
 
+	var grid = $('.grid');
+
+	grid.masonry({
+		itemSelector: '.grid-item',
+		columnWidth: 200
+	});
+
+	grid.infinitescroll({
+		// Pagination element that will be hidden
+		navSelector: '#pagination',
+
+		// Next page link
+		nextSelector: '#pagination p a',
+
+		// Selector of items to retrieve
+		itemSelector: '.grid-item',
+
+		// Loading message
+		loadingText: 'Loading new items…'
+	},
+
+	// Function called once the elements are retrieved
+	function(new_elts) {
+		var elts = $(new_elts).css('opacity', 0);
+
+		elts.animate({opacity: 1});
+		grid.masonry('appended', elts);
+	});
+
+	$(window).unbind('.infscr');
+
+	$('.foo').click(function(){
+		$('.grid').infinitescroll('retrieve');
+		return false;
+	});
+
 });
