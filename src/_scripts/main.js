8275b1a86be3761fddc5b951196053aeb02c771d
Michael <michael.lai@adelphi.digital>
2016-06-21 23:49:16 +1000

    social float progress

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -183,4 +183,32 @@ $(() => {
 		$(this).fadeOut();
 	});
 
+	if ($('.sticky').length) { // make sure ".sticky" element exists
+		var el = $('.sticky');
+		var stickyTop = $('.sticky').offset().top; // returns number
+		var stickyHeight = $('.sticky').height();
+
+		$(window).scroll(function() { // scroll event
+			var limit = $('.sticky-stop').offset().top - stickyHeight - 20;
+
+			var windowTop = $(window).scrollTop(); // returns number
+
+			if (stickyTop < windowTop) {
+				el.css({
+					position: 'fixed',
+					top: 0
+				});
+			} else {
+				el.css('position', 'static');
+			}
+
+			if (limit < windowTop) {
+				var diff = limit - windowTop;
+				el.css({
+					top: diff
+				});
+			}
+		});
+	}
+
 });
